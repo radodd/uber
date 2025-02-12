795a1db4f287ccc4beb4538b4445ef95f40bf9df
radodd <ethanf.flores@gmail.com>
2025-02-11 22:59:11 -0500

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>HOme</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
